aeae5a948e368e4a7ace9d8c0feca68e3f70af77
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2022-02-17 09:50:03 +0100

    Fixed some styles

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
     .copyFiles({
 		from: './assets/images', to: 'images/[path][name].[hash:8].[ext]'
 	})    
